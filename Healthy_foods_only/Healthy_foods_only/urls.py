237103c894cfb7aabbc0b48e4d34957011e1c8da
Anushivakoti <shivakotianu2@gmail.com>
2021-08-29 14:53:34 +0545

    Project setup

---
+++ Healthy_foods_only/Healthy_foods_only/urls.py
@@ -0,0 +1,10 @@
+
+
+from django.urls import path, include
+
+urlpatterns = [
+    path('foods/', include('foods.urls')),
+    path('admins/', include('admins.urls')),
+    path('', include('accounts.urls'))
+]
+
